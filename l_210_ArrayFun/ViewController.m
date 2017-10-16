0333591da931c0f6beb107b30cdf749db29c5cc8
tintedsky <hongbonew@gmail.com>
2017-10-16 00:08:25 -0400

    Array

--- l_210_ArrayFun/ViewController.m
+++ l_210_ArrayFun/ViewController.m
@@ -16,7 +16,39 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    // NSArray *arr = [[NSArray alloc]init];
+    //Do not use this way because NSArray cannot be modified after initialization.
+    
+    
+    //arr[0] = str; XXX wrong
+
+    NSArray *arr = [NSArray arrayWithObjects:@"My", @"look", @"does", nil];
+    
+    NSLog(@"The second element: %@", arr[1]);
+    
+    NSMutableArray *mut = [NSMutableArray arrayWithObjects:@"Mutable", @"Array", @"being", @"under", @"test", nil];
+    //NSLog(@"The second element: %@", mut[2]);
+    NSLog(@"Mut original: %@", mut.debugDescription);
+    [mut removeObjectAtIndex:2];
+    //NSLog(@"The second element: %@", arr[2]);
+    [mut addObject:@"Beast Monster"];
+    NSLog(@"Mut modified: %@", mut.debugDescription);
+    
+    NSArray *arr2 = @[@"Donkey", @"Kong", @"Ate", @"Test"];
+    
+    NSString *str = [arr2 objectAtIndex:2];
+    
+    arr2 = @[@"replace", @"Array"]; // Works??? surprising. Memory leak?
+    
+    //Create a new array initialized with the elements of another array.
+    NSArray *arr3 = [NSArray arrayWithArray:arr2];
+    
+    NSArray *arr4 = mut; //Can use mutable array to initialize NSArray
+    
+    //NSMutableArray *arr5 = arr3;         // incompatible pointers
+    
+    
 }
 
 
